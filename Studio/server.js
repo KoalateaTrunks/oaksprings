f34cb760401ebec4add1a9afe81dbd11459b57c0
Max <39538947+pupperfishes@users.noreply.github.com>
2018-05-23 15:15:00 -0700

    capitals

--- Studio/server.js
+++ Studio/server.js
@@ -44,11 +44,11 @@ net.createServer(function(socket) {
     thisClient.socket = socket;
     thisClient.initiate();
 
-    socket.on('error', thisclient.error);
+    socket.on('error', thisClient.error);
 
     socket.on('end', thisClient.end);
 
-    socket.on('data', thisclient.data);
+    socket.on('data', thisClient.data);
 
 }).listen(config.port);
 
