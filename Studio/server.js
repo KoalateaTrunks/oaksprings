b40ac2f9777379cfe1f79b0568d98b08b8ca7e88
VoxRatio <vox@whirlwhinds.com>
2018-05-23 18:23:33 +1000

    Fix Server
    Bunch of finnicky errors fixed

--- Studio/server.js
+++ Studio/server.js
@@ -1,14 +1,14 @@
 /** 5/22/2018 */
 //import req libraries
 
-require(__dirname + '/Resources/config.js');
+var config = require(__dirname + '/Resources/config.js');
 var fs = require('fs');
 //fs = file system
 var net = require('net');
 //net = gives access to creating servers, listening to events, etc
 
 //load initialisers
-var init_files = fs.readdirSync(__dirname + "/Initializer"); // code will not move on before this piece 
+var init_files = fs.readdirSync(__dirname + "/Initializer"); // code will not move on before this piece
 init_files.forEach(function(initFile)
 /**for every item in array call this func */ {
     console.log('Loading Initializer: ' + initFile);
@@ -16,7 +16,7 @@ init_files.forEach(function(initFile)
 });
 
 //load models
-var model_files = fs.readdirSync(__dirname + "/Models"); 
+var model_files = fs.readdirSync(__dirname + "/Models");
 model_files.forEach(function(modelFile)
 // for every item in array call this func
  {
@@ -30,10 +30,10 @@ var map_files = fs.readdirSync(config.data_paths.maps);
 map_files.forEach(function(mapFile)
 {
     console.log('Loading Map: ' + mapFile);
-    require(__dirname + config.data_paths.maps + mapFile);
+    require(config.data_paths.maps + mapFile);
     var map = require(config.data_paths.maps + mapFile);
     maps[map.room] = map
-}); 
+});
 
 net.createServer(function(socket) {
 
@@ -56,4 +56,3 @@ net.createServer(function(socket) {
 }).listen(config.port);
 
 console.log("initialize completed, server running on port:" +config.port + " for environment: " + config.environment);
-
